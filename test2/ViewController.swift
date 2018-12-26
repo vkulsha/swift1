649b802444b146e006f0e585e76e17d82bd88ac0
vkulsha <vkulsha@mail.ru>
2018-12-26 20:29:10 +0300

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Птичка on 26/12/2018.
+//  Copyright © 2018 Птичка. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
