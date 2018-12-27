853258280687168fc34b6729b0036e2594e67e06
vkulsha <vkulsha@mail.ru>
2018-12-27 19:04:43 +0300

    init

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,6 +16,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func bclick(_ sender: Any) {
+        let url = URL(string: "http://kulsha.ru/php/olp.php?f=gN&p=[2]")!
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+        }
+        task.resume()
+        
+        let data = ["123", "234", "345", "Слава", "ewer", "reer", "werew"]
+        (children[0] as? InfoTableViewController)?.modelArray = data
+        (children[0] as? InfoTableViewController)?.tableView.reloadData()
+        
+    }
+    
 }
 
+class Test: Aaa {
+    var b: Int
+    
+    func getB() -> Int {
+        return self.b
+    }
+    
+    init () {
+        self.b = 20
+    }
+
+    
+}
+
+protocol Aaa {
+    var b : Int { get }
+    func getB() -> Int
+}
